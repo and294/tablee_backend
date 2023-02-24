e5916fe39941559c07dc8fed480035487fb271dc
Joachim Jasmin <joachim.jasmin@gmail.com>
2023-02-24 11:45:30 +0100

    user schema updated

--- models/users.js
+++ models/users.js
@@ -1,15 +1,24 @@
 const mongoose = require("mongoose");
 
+const creditCardSchema = mongoose.Schema({
+  name: String,
+  number: Number,
+  expirationDate: Date, // format: MM/YY
+  cvc: Number,
+});
+
 const userSchema = mongoose.Schema({
   username: String,
   firstname: String,
   email: String,
   password: String,
-  picture: String,
-  bio: String,
-  studentCard: String,
-  creditCard: Object,
   token: String,
+  picture: String,
+  studentCard: String,
+  bio: String,
+  creditCard: creditCardSchema,
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "restaurants" }],
+  history: [{ type: mongoose.Schema.Types.ObjectId, ref: "bookings" }],
 });
 
 const User = mongoose.model("users", userSchema);
