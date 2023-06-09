348302ba252f27d8400be61f8af31572c9ddebe0
Andrea <and29ferraro@gmail.com>
2023-06-09 18:46:02 +0200

    first commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-//. MongoDB
+///. MongoDB
 require("./models/connection");
 
 //. File upload
